b732134f4edab1371656343ebc2c961a7b8f565d
RogerChao <chaoshuangjui@gmail.com>
2015-10-22 14:50:31 +0800

    ASP.NET MVC 專案分層架構 Part.1 初學者的起手式

---
+++ MultiLayerProj/Mvc_Repository/Mvc_Repository/Controllers/CategoriesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Mvc_Repository.Models;
+using Mvc_Repository.Models.Interface;
+
+namespace Mvc_Repository.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository categoryRepository;
+
+        public CategoryController()
+        {
+            this.categoryRepository = new CategoryRepository();
+        }
+
+
+        public ActionResult Index()
+        {
+            var categories = this.categoryRepository.GetAll().ToList();
+            return View(categories);
+        }
+
+        //=========================================================================================
+
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                var category = this.categoryRepository.Get(id.Value);
+                return View(category);
+
+            }
+        }
+
+        //=========================================================================================
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categories category)
+        {
+            if (category != null && ModelState.IsValid)
+            {
+                this.categoryRepository.Create(category);
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+
+        //=========================================================================================
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                var category = this.categoryRepository.Get(id.Value);
+                return View(category);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Categories category)
+        {
+            if (category != null && ModelState.IsValid)
+            {
+                this.categoryRepository.Update(category);
+                return View(category);
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
+        }
+
+        //=========================================================================================
+
+        public ActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                var category = this.categoryRepository.Get(id.Value);
+                return View(category);
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                var category = this.categoryRepository.Get(id);
+                this.categoryRepository.Delete(category);
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Delete", new { id = id });
+            }
+            return RedirectToAction("index");
+        }
+
+    }
+}
