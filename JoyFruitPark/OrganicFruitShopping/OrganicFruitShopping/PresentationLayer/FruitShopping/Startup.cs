4b3328e74b096e21ba60e166f32f1c973a430a98
RogerChao <chaoshuangjui@gmail.com>
2015-10-23 14:01:08 +0800

    http://mvc.keigen.net/2013/12/2-4-aspnet-web.html(2-4 ASP.NET Web 應用程式)

---
+++ JoyFruitPark/OrganicFruitShopping/OrganicFruitShopping/PresentationLayer/FruitShopping/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FruitShopping.Startup))]
+namespace FruitShopping
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
