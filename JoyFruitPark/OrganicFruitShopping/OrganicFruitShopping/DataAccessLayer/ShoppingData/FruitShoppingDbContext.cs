5c45864eb5108ca0c5eab4b6dc15b51da31ef2d1
chao1017 <chaoshuangjui@gmail.com>
2016-01-11 10:32:46 +0800

    rebuild

---
+++ JoyFruitPark/OrganicFruitShopping/OrganicFruitShopping/DataAccessLayer/ShoppingData/FruitShoppingDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingBusiness;
+using System.Data.Entity;
+
+namespace ShoppingData
+{
+    public class FruitShoppingDbContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+    }
+}
---
+++ JoyFruitPark/OrganicFruitShopping/OrganicFruitShopping/PresentationLayer/FruitShopping/Controllers/FruitShoopingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingData;
+
+namespace FruitShopping.Controllers
+{
+    public class FruitShoopingController : Controller
+    {
+        private FruitShoppingDbContext db = new FruitShoppingDbContext();
+        
+        // GET: FruitShooping
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
