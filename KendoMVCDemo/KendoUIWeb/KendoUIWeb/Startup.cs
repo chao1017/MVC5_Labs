b32898564598a31c1df9b1084b4a14e16c10d1a4
chao1017 <chaoshuangjui@gmail.com>
2016-07-13 09:27:04 +0800

    Add Kendo UI Grid Esample

---
+++ KendoMVCDemo/KendoUIWeb/KendoUIWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KendoUIWeb.Startup))]
+namespace KendoUIWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
