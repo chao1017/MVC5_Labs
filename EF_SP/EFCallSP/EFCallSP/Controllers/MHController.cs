768d7051ce93ed922d191f92ec99de7cd3d6326f
chao1017 <chaoshuangjui@gmail.com>
2016-07-18 13:37:59 +0800

    test call store procedure

---
+++ EF_SP/EFCallSP/EFCallSP/Controllers/MHController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCallSP.Models;
+
+namespace EFCallSP.Controllers
+{
+    public class MHController : Controller
+    {
+        private CTCI_MH_DBEntities db = new CTCI_MH_DBEntities();
+
+        // GET: MH
+        public ActionResult Index()
+        {
+            var result = db.s_gmh_get_leave_form_data("54CA389E-2A66-4869-BDB1-E933505A3678", "9933");
+
+            return View(result);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EF_SP/EFCallSP/EFCallSP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EFCallSP.Startup))]
+namespace EFCallSP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
