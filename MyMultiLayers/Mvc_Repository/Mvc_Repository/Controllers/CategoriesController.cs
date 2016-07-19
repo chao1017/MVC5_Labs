237bcc089270637a92ee1fe9aa5f5edfb0e05114
chao1017 <chaoshuangjui@gmail.com>
2016-07-19 15:54:46 +0800

    http://kevintsengtw.blogspot.tw/2012/10/aspnet-mvc-part2-repository.html

---
+++ MyMultiLayers/Mvc_Repository/Mvc_Repository/Controllers/CategoriesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Mvc_Repository.Models;
+using Mvc_Repository.Models.Interface;
+using Mvc_Repository.Models.Repository;
+
+namespace Mvc_Repository.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private IRepository<Categories> categoriesRepository;
+
+        public CategoryController()
+        {
+            this.categoriesRepository = new GenericRepository<Categories>();
+        }
+
+
+        public ActionResult Index()
+        {
+            var categories = this.categoriesRepository.GetAll().OrderByDescending(x => x.CategoryID).ToList();
+            return View(categories);
+        }
+
+        //=========================================================================================
+
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                var category = this.categoriesRepository.Get(x => x.CategoryID == id.Value);
+                return View(category);        
+            }
+        }
+
+        //=========================================================================================
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Categories category)
+        {
+            if (category != null && ModelState.IsValid)
+            {
+                this.categoriesRepository.Create(category);
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+        
+        ////=========================================================================================
+        
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                var category = this.categoriesRepository.Get(x => x.CategoryID == id.Value);
+                return View(category);
+            }
+        }
+        
+        [HttpPost]
+        public ActionResult Edit(Categories category)
+        {
+            if (category != null && ModelState.IsValid)
+            {
+                this.categoriesRepository.Update(category);
+                return View(category);
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
+        }
+        
+        ////=========================================================================================
+        
+        public ActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                var category = this.categoriesRepository.Get(x => x.CategoryID == id.Value);
+                return View(category);
+            }
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                var category = this.categoriesRepository.Get(x => x.CategoryID == id);
+                this.categoriesRepository.Delete(category);
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Delete", new { id = id });
+            }
+            return RedirectToAction("index");
+        }
+
+    }
+}
---
+++ MyMultiLayers/Mvc_Repository/Mvc_Repository/Controllers/ProductsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Repository.Models;
+using Mvc_Repository.Models.Interface;
+using Mvc_Repository.Models.Repository;
+
+namespace Mvc_Repository.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IRepository<Products> productsRepository;
+        private IRepository<Categories> categoriesRepository;
+
+        public IEnumerable<Categories> Categories
+        {
+            get 
+            {
+                return categoriesRepository.GetAll();
+            }
+        }
+ 
+        public ProductsController()
+        {
+            this.productsRepository = new GenericRepository<Products>();
+            this.categoriesRepository = new GenericRepository<Categories>();
+        }
+ 
+        public ActionResult Index()
+        {
+            var products = productsRepository.GetAll().OrderByDescending(x => x.ProductID).ToList();
+            return View(products);
+        }
+ 
+        //=========================================================================================
+        
+        public ActionResult Details(int id = 0)
+        {
+            Products product = productsRepository.Get(x => x.ProductID == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+ 
+        //=========================================================================================
+ 
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(this.Categories, "CategoryID", "CategoryName");
+            return View();
+        }
+ 
+        [HttpPost]
+        public ActionResult Create(Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                this.productsRepository.Create(products);
+                return RedirectToAction("Index");
+            }
+        
+            ViewBag.CategoryID = new SelectList(this.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            return View(products);
+        }
+ 
+        //=========================================================================================
+ 
+        public ActionResult Edit(int id = 0)
+        {
+            Products product = this.productsRepository.Get(x => x.ProductID == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(this.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+ 
+        [HttpPost]
+        public ActionResult Edit(Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                this.productsRepository.Update(products);
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(this.Categories, "CategoryID", "CategoryName", products.CategoryID);
+            return View(products);
+        }
+ 
+        //=========================================================================================
+ 
+        public ActionResult Delete(int id = 0)
+        {
+            Products product = this.productsRepository.Get(x => x.ProductID == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+ 
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Products product = this.productsRepository.Get(x=>x.ProductID == id);
+            this.productsRepository.Delete(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
