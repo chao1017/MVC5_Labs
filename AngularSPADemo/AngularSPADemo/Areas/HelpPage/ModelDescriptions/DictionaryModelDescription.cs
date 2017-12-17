bd07fe1e81bbc8a84b90f57a7ccee3c5d9ac08af
roerchao <chaosguangjui@gmail.com>
2017-12-18 00:39:34 +0800

    first SPA + AngularJS

---
+++ AngularSPADemo/AngularSPADemo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AngularSPADemo.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AngularSPADemo/AngularSPADemo/Scripts/angular.SPADemo.js
@@ -0,0 +1,11 @@
+﻿var SPA = angular.module("SPA", ["ngRoute"]);
+
+//SPA路由設定，可在此往下新增路由
+SPA.config(['$routeProvider', "$locationProvider",
+    function ($routeProvider, $locationProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl: '/Home/SPApage1',
+            controller: 'Main'
+        })
+    }])
